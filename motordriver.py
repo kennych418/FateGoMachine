e4264bcbce6a98c0020b38dcf410786a531498a9
Kenny Chan <chankenny900@gmail.com>
2020-07-20 22:54:55 -0700

    Code for driving stepper motor

---
+++ motordriver.py
@@ -0,0 +1,67 @@
+#Funtions to control 28BYJ-48 stepper motor with ULN2003AN driver board 
+import RPi.GPIO as GPIO 
+int STEPPER_PIN_1 = 32
+int STEPPER_PIN_2 = 36
+int STEPPER_PIN_3 = 38
+int STEPPER_PIN_4 = 40
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(STEPPER_PIN_1, GPIO.OUT)
+GPIO.setup(STEPPER_PIN_2, GPIO.OUT)
+GPIO.setup(STEPPER_PIN_3, GPIO.OUT)
+GPIO.setup(STEPPER_PIN_4, GPIO.OUT)
+
+int step_number = 0
+
+def OneStep(bool dir):
+    global step_number
+    
+    if(dir) and (step_number == 0):
+        GPIO.output(STEPPER_PIN_1, 1)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 0)
+    elif(dir) and (step_number == 1):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 1)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 0)
+    elif(dir) and (step_number == 2):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 1)
+        GPIO.output(STEPPER_PIN_4, 0)
+    elif(dir) and (step_number == 3):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 1)
+    elif(not dir) and (step_number == 0):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 1)
+    elif(not dir) and (step_number == 1):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 1)
+        GPIO.output(STEPPER_PIN_4, 0)
+    elif(not dir) and (step_number == 2):
+        GPIO.output(STEPPER_PIN_1, 0)
+        GPIO.output(STEPPER_PIN_2, 1)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 0)
+    elif(not dir) and (step_number == 3):
+        GPIO.output(STEPPER_PIN_1, 1)
+        GPIO.output(STEPPER_PIN_2, 0)
+        GPIO.output(STEPPER_PIN_3, 0)
+        GPIO.output(STEPPER_PIN_4, 0)
+
+    step_number = step_number + 1
+    if(step_number > 3):
+        step_number = 0
+}
+
+if __name__ == "__main__":
+    OneStep(false)
+    delay(2)
